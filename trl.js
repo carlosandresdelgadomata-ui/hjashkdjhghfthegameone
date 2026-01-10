ea79d5930bdf11c59c4e56ff20440fd6e0e7c7bd
carlosandresdelgadomata-ui <carlosandresdelgadomata@gmail.com>
2026-01-10 15:59:22 +0100

    Delete trl.js

--- trl.js
+++
@@ -1,2 +0,0 @@
-// trl.js
-const webhookUrl = 'https://discord.com/api/webhooks/1457070998451917054/6MI5ggtPTijbdQQ4xHAP3jkcs_40oP5WhiOJ7B2z507bf-uV_qqKGzrTjpvJ57T-0iRH'; // Reemplaza esto con tu Webhook real
